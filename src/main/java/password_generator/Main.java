28bbaab191837ed70ee97b3596210b6c2ddcb611
Zeki Kadiroğlu <z.kdrglu07@gmail.com>
2022-08-15 13:18:45 +0300

    Password-Generator

---
+++ src/main/java/password_generator/Main.java
@@ -0,0 +1,14 @@
+package password_generator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static final Scanner keyboard = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Generator generator = new Generator(keyboard);
+        generator.mainLoop();
+        keyboard.close();
+    }
+}
